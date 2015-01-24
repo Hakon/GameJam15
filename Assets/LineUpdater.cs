fe56317bdab0abdbd887b4916db91b5cd1478c49
VemundY <Vemundytrehus@gmail.com>
2015-01-24 20:34:45 +0100

    Revert "Lagt til skript for kollisjon"
    This reverts commit 96a19d9a8ef6a783a9b8b0b1f3e265fadd6f843c.

--- Assets/LineUpdater.cs
+++ Assets/LineUpdater.cs
@@ -22,10 +22,11 @@ public class LineUpdater : MonoBehaviour {
 		
 		Ray ray = new Ray(object1.transform.position, object2.transform.position - object1.transform.position);
 		RaycastHit hit;
-		if(Physics.Raycast(ray, out hit, 100f, LayerMask.GetMask("Hinder"))){
-			object1.transform.position = prevPos1;
-			object2.transform.position = prevPos2;
-			Debug.Log ("Hinder!");
+		if(Physics.Raycast(ray, out hit, 100f)){
+			if(hit.transform.gameObject.tag == "Van"){
+				object1.transform.position = prevPos1;
+				object2.transform.position = prevPos2;
+			}
 		}
 
 		prevPos1 = object1.transform.position;
