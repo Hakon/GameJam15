306908e6bb0651a314c41de387b0fb7978d7b366
Håkon Lerring <hakon@powow.no>
2015-01-24 19:14:19 +0100

    Refactor line renderer

--- Assets/LineUpdater.js
+++ Assets/LineUpdater.js
@@ -2,13 +2,14 @@
 
 public var object1 : Transform;
 public var object2 : Transform;
+var linerenderer : LineRenderer;
 
 function Start () {
 
+	linerenderer = GetComponent(LineRenderer);
 }
 
 function Update () {
-	var linerenderer : LineRenderer = GetComponent(LineRenderer);
 	
 	linerenderer.SetPosition(0, object1.transform.position);
 	linerenderer.SetPosition(1, object2.transform.position);
