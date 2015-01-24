4f2a10e1edfd35af8b9fbc21adede2435c5591e8
VemundY <Vemundytrehus@gmail.com>
2015-01-24 16:45:44 +0100

    La til politiet og trigger for game over om man kommer i kontakt

--- Assets/EventHandler.js
+++ Assets/EventHandler.js
@@ -2,8 +2,16 @@
 
 function OnControllerColliderHit(hit : ControllerColliderHit)
 {
-    if(hit.collider.CompareTag ("Police"))
+    if(hit.collider.CompareTag ("Van"))
     {
     	// game over
 	}	
+}
+
+function OnTriggerEnter(col : Collider)
+{
+	if(col.CompareTag("Police"))
+	{
+    	Debug.Log("Game Over");
+	}
 }
