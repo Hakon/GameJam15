bee7c4b3f3704b1355cf777d0754c03fb42530fb
Håkon Lerring <hakon@powow.no>
2015-01-24 15:04:34 +0100

    Initial Commit

---
+++ Assets/ConstantSpeedMover.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+
+public var Direction : Vector3;
+
+function Start () {
+
+}
+
+function Update () {
+	transform.position = transform.position + Direction;
+}
---
+++ Assets/LineUpdater.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+public var object1 : Transform;
+public var object2 : Transform;
+
+function Start () {
+
+}
+
+function Update () {
+	var linerenderer : LineRenderer = GetComponent(LineRenderer);
+	
+	linerenderer.SetPosition(0, object1.transform.position);
+	linerenderer.SetPosition(1, object2.transform.position);
+}
