226674e443ee7f66f8e1799e73e35c5196fa4141
EivindLervik <eivind_fantaboy@msn.com>
2015-01-24 20:08:56 +0100

    Fikset collision detection

---
+++ Assets/LineUpdater.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LineUpdater : MonoBehaviour {
+
+	public GameObject object1;
+	public GameObject object2;
+	LineRenderer linerenderer;
+
+	private Vector3 prevPos1;
+	private Vector3 prevPos2;
+	
+	void Start () {
+		
+		linerenderer = gameObject.GetComponent<LineRenderer>();
+	}
+	
+	void Update () {
+		
+		linerenderer.SetPosition(0, object1.transform.position);
+		linerenderer.SetPosition(1, object2.transform.position);
+		
+		Ray ray = new Ray(object1.transform.position, object2.transform.position - object1.transform.position);
+		RaycastHit hit;
+		if(Physics.Raycast(ray, out hit, 100f)){
+			if(hit.transform.gameObject.tag == "Van"){
+				object1.transform.position = prevPos1;
+				object2.transform.position = prevPos2;
+			}
+		}
+
+		prevPos1 = object1.transform.position;
+		prevPos2 = object2.transform.position;
+	}
+}
